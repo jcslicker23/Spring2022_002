dab5d9cf79099c1c265053f17c3b688848baaa9e
jcslicker23 <99768787+jcslicker23@users.noreply.github.com>
2022-03-01 18:28:32 -0600

    Add files via upload
    This is my change to lab 3

---
+++ TravelDestination.java
@@ -0,0 +1,25 @@
+public class TravelDestination {
+	public static void main(String[] args) {
+		System.out.println("My name is Heena Rathore. I wish to travel to Hawaii one day !");
+		System.out.println("My name is Benjamin Lindley. I wish to travel to Jupiter one day.");
+		System.out.println("My name is Garreth Shonaike. I wish to travel to Seoul one day!");
+		System.out.println("My name is Sabrina Ramon. I wish to travel to Japan one day!");
+		System.out.println("My name is Gia German. I wish to travel to Italy one day!");
+		System.out.println("My name is Kaleb Galvan. I wish to travel to Australia one day!");
+		System.out.println("My name is Aldo Martinez. I wish to travel to Rome, Italy one day!");
+		System.out.println("My name is Isabella Montemayor. I wish to travel to Greece one day!");
+		System.out.println("My name is Lydia Unterreiner. I wish to travel to Peru one day!");
+		System.out.println("My name is Jeffrey Cordes. I wish to travel to Japan one day !");
+		System.out.println("My name is Alberto Arias. I wish to travel to Australia one day !");
+		System.out.println("My name is Aidan Chang. I wish to travel to Taiwan one day !");
+		System.out.println("My name is Pete Acevedo. I wish to travel to Mexico one day !");
+		System.out.println("My name is Noah Mesa. I wish to travel to Ohio one day !");
+		System.out.println("My name is John Oranday. I wish to travel to Japan one day !");
+		System.out.println("My name is Timothy Armstrong. I wish to travel to Tokyo one day !");
+
+		System.out.println("My name is Melwyn Cespedes. I wish to travel to Brazil one day !");
+		System.out.println("My name is Jorge Gonzalez. I wish to travel to Switzerland one day!");
+                        System.out.println("My name is Rizwan khan.I wish to travel to France one day !");
+}
+
+}
